e7a5f9b5b1f1bda5f45aee27604e497f7b01f78f
barakgahtan <barakgahtan@gmail.com>
2020-08-22 16:40:24 +0300

    prepare data sets for corolations

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import main_data as dataLoader
 from cs236781 import plot
 from training import VAETrainer
 torch.manual_seed(42)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 
@@ -37,6 +38,74 @@ z_dim = hp['z_dim']
 x_sigma2 = hp['x_sigma2']
 learn_rate = hp['learn_rate']
 betas = hp['betas']
+
+#preparing different datasets
+# 1 - aux sensor only
+data_aux = dataLoader.DatasetCombined(1)
+split_lengths_aux = [int(len(data_aux)*0.6+1), int(len(data_aux)*0.4)]
+ds_train_aux, ds_test_aux = random_split(data_aux, split_lengths_aux)
+dl_train_aux = DataLoader(ds_train_aux, batch_size=4, shuffle=True)
+dl_test_aux = DataLoader(ds_test_aux, batch_size=4, shuffle=True)
+
+# 2 - thermal sensor only
+data_thermal = dataLoader.DatasetCombined(2)
+split_lengths_thermal = [int(len(data_thermal)*0.6+1), int(len(data_thermal)*0.4)]
+ds_train_thermal, ds_test_thermal = random_split(data_thermal, split_lengths_thermal)
+dl_train_thermal = DataLoader(ds_train_thermal, batch_size=4, shuffle=True)
+dl_test_thermal = DataLoader(ds_test_thermal, batch_size=4, shuffle=True)
+
+# 3 - energy sensor only
+data_energy = dataLoader.DatasetCombined(3)
+split_lengths_energy = [int(len(data_energy)*0.6+1), int(len(data_energy)*0.4)]
+ds_train_energy, ds_test_energy = random_split(data_energy, split_lengths_energy)
+dl_train_energy = DataLoader(ds_train_energy, batch_size=4, shuffle=True)
+dl_test_energy = DataLoader(ds_test_energy, batch_size=4, shuffle=True)
+
+# 4 - thermal and aux
+data_thermal_aux = dataLoader.DatasetCombined(4)
+split_lengths_thermal_aux = [int(len(data_thermal_aux)*0.6+1), int(len(data_thermal_aux)*0.4)]
+ds_train_thermal_aux, ds_test_thermal_aux = random_split(data_thermal_aux, split_lengths_thermal_aux)
+dl_train_thermal_aux = DataLoader(ds_train_thermal_aux, batch_size=4, shuffle=True)
+dl_test_thermal_aux = DataLoader(ds_test_thermal_aux, batch_size=4, shuffle=True)
+
+# 5 - energy and aux
+data_energy_aux = dataLoader.DatasetCombined(5)
+split_lengths_energy_aux = [int(len(data_energy_aux)*0.6+1), int(len(data_energy_aux)*0.4)]
+ds_train_energy_aux, ds_test_energy_aux = random_split(data_energy_aux, split_lengths_energy_aux)
+dl_train_energy_aux = DataLoader(ds_train_energy_aux, batch_size=4, shuffle=True)
+dl_test_energy_aux = DataLoader(ds_test_energy_aux, batch_size=4, shuffle=True)
+
+# 6 - energy and thermal
+data_energy_thermal = dataLoader.DatasetCombined(6)
+split_lengths_energy_thermal = [int(len(data_energy_thermal)*0.6+1), int(len(data_energy_thermal)*0.4)]
+ds_train_energy_thermal, ds_test_energy_thermal = random_split(data_energy_thermal, split_lengths_energy_thermal)
+dl_train_energy_thermal = DataLoader(ds_train_energy_thermal, batch_size=4, shuffle=True)
+dl_test_energy_thermal = DataLoader(ds_test_energy_thermal, batch_size=4, shuffle=True)
+
+# 7 - energy, thermal and aux
+data_energy_thermal_aux = dataLoader.DatasetCombined(7)
+split_lengths_energy_thermal_aux = [int(len(data_energy_thermal_aux)*0.6+1), int(len(data_energy_thermal_aux)*0.4)]
+ds_train_energy_thermal_aux, ds_test_energy_thermal_aux = random_split(data_energy_thermal_aux, split_lengths_energy_thermal_aux)
+dl_train_energy_thermal_aux = DataLoader(ds_train_energy_thermal_aux, batch_size=4, shuffle=True)
+dl_test_energy_thermal_aux = DataLoader(ds_test_energy_thermal_aux, batch_size=4, shuffle=True)
+
+i = 0
+list_DL_tuples = []
+list_DL_tuples.append( (dl_train_aux, dl_test_aux))
+list_DL_tuples.append( (dl_train_thermal, dl_test_thermal))
+list_DL_tuples.append( (dl_train_energy, dl_test_energy))
+list_DL_tuples.append( (dl_train_thermal_aux, dl_test_thermal_aux))
+list_DL_tuples.append( (dl_train_energy_aux, dl_test_energy_aux))
+list_DL_tuples.append( (dl_train_energy_thermal, dl_test_energy_thermal))
+list_DL_tuples.append( (dl_train_energy_thermal_aux, dl_test_energy_thermal_aux))
+
+# Model
+encoder = auto_encoder.EncoderCNN()
+decoder = auto_encoder.DecoderCNN()
+
+
+
+
 
 #Data
 data = dataLoader.DatasetCombined()
@@ -66,8 +135,7 @@ decoded = decoder(enc_feedforward) #shape is 3,1,314 ( back to normal)
 vae = autoencoder.VAE(encoder, decoder, raw_sample.shape, 1)#(features_encoder, features_decoder, in_size, z_dim)
 vae_dp = DataParallel(vae)
 z, mu, log_sigma2 = vae.encode(dataload_sample)
-# Optimizer
-optimizer = optim.Adam(vae.parameters(), lr=learn_rate, betas=betas)
+
 
 # Loss
 def loss_fn_mse(x, xr, z_mu, z_log_sigma2):
@@ -75,23 +143,17 @@ def loss_fn_mse(x, xr, z_mu, z_log_sigma2):
 
 def loss_fn_CE(x, xr, z_mu, z_log_sigma2):
     return autoencoder.vae_loss_CE(x, xr, z_mu, z_log_sigma2, x_sigma2)
-
+# Optimizer
+optimizer = optim.Adam(vae.parameters(), lr=learn_rate, betas=betas)
 # Trainer
 trainer_mse = VAETrainer(vae_dp, loss_fn_mse, optimizer, device)
 trainer_CE = VAETrainer(vae_dp, loss_fn_CE, optimizer, device)
 
-#
-
 # test_vae_loss()
-
 checkpoint_file = 'checkpoints/vae'
 checkpoint_file_final = f'{checkpoint_file}_final'
 if os.path.isfile(f'{checkpoint_file}.pt'):
     os.remove(f'{checkpoint_file}.pt')
-
-# Show model and hypers
-print(vae)
-print(hp)
 
 def post_epoch_fn(epoch, train_result, test_result, verbose):
     # Plot some samples if this is a verbose epoch
@@ -106,14 +168,23 @@ if os.path.isfile(f'{checkpoint_file_final}.pt'):
     print(f'*** Loading final checkpoint file {checkpoint_file_final} instead of training')
     checkpoint_file = checkpoint_file_final
 else:
-    res = trainer_mse.fit(dl_train, dl_test,
-                      num_epochs=5, early_stopping=20, print_every=10,
-                      checkpoints=checkpoint_file,
-                      post_epoch_fn=post_epoch_fn)
-    res = trainer_CE.fit(dl_train, dl_test,
-                      num_epochs=5, early_stopping=20, print_every=10,
-                      checkpoints=checkpoint_file,
-                      post_epoch_fn=post_epoch_fn)
+    for i in range(len(list_DL_tuples)):
+        if os.path.isfile(f'{checkpoint_file_final}.pt'):
+            print(f'*** Loading final checkpoint file {checkpoint_file_final} instead of training')
+            current_check_point_file = f'{checkpoint_file_final}_' + str(i)
+        res = trainer_mse.fit(list_DL_tuples[i][0], list_DL_tuples[i][1],
+                              num_epochs=200, early_stopping=20, print_every=10,
+                              checkpoints=checkpoint_file,
+                              post_epoch_fn=post_epoch_fn)
+
+    for i in range(len(list_DL_tuples)):
+        if os.path.isfile(f'{checkpoint_file_final}.pt'):
+            print(f'*** Loading final checkpoint file {checkpoint_file_final} instead of training')
+            current_check_point_file = f'{checkpoint_file_final}_' + str(i)
+        res = trainer_CE.fit(list_DL_tuples[i][0], list_DL_tuples[i][1],
+                              num_epochs=200, early_stopping=20, print_every=10,
+                              checkpoints=checkpoint_file,
+                              post_epoch_fn=post_epoch_fn)
 
 
 # enc_feedforward = enc(dl_sample) # shape is 4,1024,319
--- main_data.py
+++ main_data.py
@@ -6,27 +6,110 @@ import aux_data_sensors as aux_loader
 import thermal_energy_intersect as thermal_energy_loader
 
 
-def data_wrapper():
-    res = []
-    aux = aux_loader.return_data()
-    thermal = thermal_energy_loader.return_data(0)
-    energy = thermal_energy_loader.return_data(1)
-    for j in range(len(aux)):
-        res.append(tf.convert_to_tensor(aux[j],dtype=tf.float32)) #before it was only insert without conversion
-    for j in range(len(thermal)):
-        res.append(tf.convert_to_tensor(thermal[j],dtype=tf.float32))  #before it was only insert without conversion
-    for j in range(len(energy)):
-        res.append(tf.convert_to_tensor(energy[j],dtype=tf.float32))  #before it was only insert without conversion
-    max = tf.reduce_max(tf.stack(res))
-    min = tf.reduce_min(tf.stack(res))
-    normalized_results = []
-    for t in res:
-        normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x-min) / (max-min)), t))
-    return normalized_results
+def data_wrapper(flag):
+    if flag == 1:#only aux sensors.
+        res = []
+        aux = aux_loader.return_data()
+        for j in range(len(aux)):
+            res.append(tf.convert_to_tensor(aux[j],dtype=tf.float32)) #before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x - min) / (max - min)), t))
+        return normalized_results
+    if flag == 2:#only thermal sensors.
+        res = []
+        thermal = thermal_energy_loader.return_data(0)
+        for j in range(len(thermal)):
+            res.append(tf.convert_to_tensor(thermal[j], dtype=tf.float32))  # before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x - min) / (max - min)), t))
+        return normalized_results
+    if flag == 3:#only energy sensors.
+        res = []
+        energy = thermal_energy_loader.return_data(1)
+        for j in range(len(energy)):
+            res.append(tf.convert_to_tensor(energy[j],dtype=tf.float32))  #before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x-min) / (max-min)), t))
+        return normalized_results
+    if flag == 4: #only thermal and aux sensors.
+        res = []
+        aux = aux_loader.return_data()
+        thermal = thermal_energy_loader.return_data(0)
+        for j in range(len(aux)):
+            res.append(tf.convert_to_tensor(aux[j], dtype=tf.float32))  # before it was only insert without conversion
+        for j in range(len(thermal)):
+            res.append(
+                tf.convert_to_tensor(thermal[j], dtype=tf.float32))  # before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x - min) / (max - min)), t))
+        return normalized_results
+    if flag == 5: #only energy and aux sensors.
+        res = []
+        aux = aux_loader.return_data()
+        energy = thermal_energy_loader.return_data(1)
+        for j in range(len(aux)):
+            res.append(tf.convert_to_tensor(aux[j], dtype=tf.float32))  # before it was only insert without conversion
+        for j in range(len(energy)):
+            res.append(
+                tf.convert_to_tensor(energy[j], dtype=tf.float32))  # before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x - min) / (max - min)), t))
+        return normalized_results
+    if flag == 6: #only energy and thermal sensors.
+        res = []
+        thermal = thermal_energy_loader.return_data(0)
+        energy = thermal_energy_loader.return_data(1)
+        for j in range(len(thermal)):
+            res.append(
+                tf.convert_to_tensor(thermal[j], dtype=tf.float32))  # before it was only insert without conversion
+        for j in range(len(energy)):
+            res.append(
+                tf.convert_to_tensor(energy[j], dtype=tf.float32))  # before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x - min) / (max - min)), t))
+        return normalized_results
+    if flag == 7:# aux, energy and thermal sensors.
+        res = []
+        aux = aux_loader.return_data()
+        thermal = thermal_energy_loader.return_data(0)
+        energy = thermal_energy_loader.return_data(1)
+        for j in range(len(aux)):
+            res.append(tf.convert_to_tensor(aux[j], dtype=tf.float32))  # before it was only insert without conversion
+        for j in range(len(thermal)):
+            res.append(
+                tf.convert_to_tensor(thermal[j], dtype=tf.float32))  # before it was only insert without conversion
+        for j in range(len(energy)):
+            res.append(
+                tf.convert_to_tensor(energy[j], dtype=tf.float32))  # before it was only insert without conversion
+        max = tf.reduce_max(tf.stack(res))
+        min = tf.reduce_min(tf.stack(res))
+        normalized_results = []
+        for t in res:
+            normalized_results.append(tf.ragged.map_flat_values(lambda x: abs((x - min) / (max - min)), t))
+        return normalized_results
+
 
 class DatasetCombined(Dataset):
-    def __init__(self):
-        self.data = data_wrapper()
+    def __init__(self, num):
+        self.data = data_wrapper(num)
 
     def __len__(self):
         return len(self.data)
